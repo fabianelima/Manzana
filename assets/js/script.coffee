7c3617219373e919e2a0e436e4a95434f104f41f
Fabiane Lima <fabianelim@gmail.com>
2018-06-19 16:54:52 -0300

    Clickable
    Módulo que insere uma área com pontos clicáveis pronto.

--- assets/js/script.coffee
+++ assets/js/script.coffee
@@ -1,10 +1,9 @@
 ###
-	       				OA TEMPLATE 4.5
+	       				  MANZANA 5
 	------------------------------------------
 				Desenvolvido em CoffeeScript
+ 							por Fabiane Lima
 
-						Código: Fabiane Lima
-	 Ilustração: André Kranz e Eduardo Stumpf
 ###
 
 # ----- Pré-carregamento das imagens ----- #
@@ -27,27 +26,91 @@ preload = (imgs) ->
 $(window).on 'load', -> preload(imgs)
 
 
-# ----- Funções e dados ----- #
+# ----- Módulos e dados ----- #
 $ ->
-	sound = false
-	data =	[]
+	sets =
+		sound: false
+		clickable: false
 
 	audio =
 		trilha: new Audio('assets/audio/trilha.mp3')
 		clique: new Audio('assets/audio/clique.mp3')
 
+		start: ->
+			if sets.sound is true
+				audio.trilha.volume = 0.6
+				audio.trilha.loop = true
+				audio.trilha.play()
+				audio.clique.play()
+				$('.audio').fadeIn()
+
 		audio: ->
 			audio.clique.play()
 
-			if sound is false
-				sound = true
+			if sets.sound is false
+				sets.sound = true
 				audio.trilha.play()
 				$('.audio').html('<img src="assets/img/audio.svg">')
 
-			else if sound is true
-				sound = false
+			else if sets.sound is true
+				sets.sound = false
 				audio.trilha.pause()
 				$('.audio').html('<img src="assets/img/audio-off.svg">')
+
+	clickarea =
+		pro: undefined
+		count: 0
+		ctrl: []
+		resp: []
+		data:	[
+			{
+				txt: '1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+				pos: [50, 30]
+			}
+			{
+				txt: '2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+				pos: [20, 20]
+			}
+			{
+				txt: '3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+				pos: [10, 30]
+			}
+			{
+				txt: '4. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+				pos: [30, 70]
+			}
+			{
+				txt: '5. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+				pos: [50, 50]
+			}]
+
+		start: ->
+			if sets.clickable is true
+				$('.content').append('<div class="clickarea"></div>')
+
+				for i in clickarea.data
+					clickarea.resp.push clickarea.data.indexOf(i)
+					$('.clickarea').append('<div></div>')
+					$('.clickarea div:nth-child(' + (clickarea.count + 1) + ')').css
+						top: clickarea.data[clickarea.count].pos[0] + '%'
+						left: clickarea.data[clickarea.count].pos[1] + '%'
+					clickarea.count++
+
+		showC: ($el) ->
+			clickarea.ctrl.push $el.index()
+			$el.css { pointerEvents: 'none', opacity: '0.6' }
+
+			$('.dimmer').fadeIn()
+			$('.modal').html(clickarea.data[$el.index()].txt + '<button class="dismiss callend">Fechar</button>')
+
+		callEnd: ->
+			if clickarea.ctrl.length isnt clickarea.data.length then return false
+			else
+				for i, j in clickarea.ctrl
+					if clickarea.ctrl.indexOf(i) is clickarea.resp[j]
+						setTimeout ->
+							func.end()
+						, 1000
 
 	func =
 		help: ->
@@ -69,17 +132,20 @@ $ ->
 			$('.dimmer').fadeOut()
 
 		start: ->
+			sets.sound = false
+			audio.start()
+
+			sets.clickable = true
+			clickarea.start()
+
 			func.dismiss()
-			sound = true
-			audio.trilha.volume = 0.6
-			audio.trilha.loop = true
-			audio.trilha.play()
-			audio.clique.play()
 			$('.content').fadeIn()
 
 
 # ----- Eventos ----- #
 	$(document).on 'click', '.audio', -> audio.audio()
+	$(document).on 'click', '.clickarea *', -> if sets.clickable is true then clickarea.showC $(this)
+	$(document).on 'click', '.dismiss', -> if sets.clickable is true then clickarea.callEnd()
 
 	$(document).on 'click', '.start', -> func.start()
 	$(document).on 'click', '.help', -> func.help()
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
 
 /*
-	       				OA TEMPLATE 4.5
+	       				  MANZANA 5
 	------------------------------------------
 				Desenvolvido em CoffeeScript
-
-						Código: Fabiane Lima
-	 Ilustração: André Kranz e Eduardo Stumpf
+ 							por Fabiane Lima
  */
 
 (function() {
@@ -37,22 +35,105 @@
   });
 
   $(function() {
-    var audio, data, func, sound;
-    sound = false;
-    data = [];
+    var audio, clickarea, func, sets;
+    sets = {
+      sound: false,
+      clickable: false
+    };
     audio = {
       trilha: new Audio('assets/audio/trilha.mp3'),
       clique: new Audio('assets/audio/clique.mp3'),
+      start: function() {
+        if (sets.sound === true) {
+          audio.trilha.volume = 0.6;
+          audio.trilha.loop = true;
+          audio.trilha.play();
+          audio.clique.play();
+          return $('.audio').fadeIn();
+        }
+      },
       audio: function() {
         audio.clique.play();
-        if (sound === false) {
-          sound = true;
+        if (sets.sound === false) {
+          sets.sound = true;
           audio.trilha.play();
           return $('.audio').html('<img src="assets/img/audio.svg">');
-        } else if (sound === true) {
-          sound = false;
+        } else if (sets.sound === true) {
+          sets.sound = false;
           audio.trilha.pause();
           return $('.audio').html('<img src="assets/img/audio-off.svg">');
+        }
+      }
+    };
+    clickarea = {
+      pro: void 0,
+      count: 0,
+      ctrl: [],
+      resp: [],
+      data: [
+        {
+          txt: '1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          pos: [50, 30]
+        }, {
+          txt: '2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          pos: [20, 20]
+        }, {
+          txt: '3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          pos: [10, 30]
+        }, {
+          txt: '4. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          pos: [30, 70]
+        }, {
+          txt: '5. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          pos: [50, 50]
+        }
+      ],
+      start: function() {
+        var i, k, len, ref, results;
+        if (sets.clickable === true) {
+          $('.content').append('<div class="clickarea"></div>');
+          ref = clickarea.data;
+          results = [];
+          for (k = 0, len = ref.length; k < len; k++) {
+            i = ref[k];
+            clickarea.resp.push(clickarea.data.indexOf(i));
+            $('.clickarea').append('<div></div>');
+            $('.clickarea div:nth-child(' + (clickarea.count + 1) + ')').css({
+              top: clickarea.data[clickarea.count].pos[0] + '%',
+              left: clickarea.data[clickarea.count].pos[1] + '%'
+            });
+            results.push(clickarea.count++);
+          }
+          return results;
+        }
+      },
+      showC: function($el) {
+        clickarea.ctrl.push($el.index());
+        $el.css({
+          pointerEvents: 'none',
+          opacity: '0.6'
+        });
+        $('.dimmer').fadeIn();
+        return $('.modal').html(clickarea.data[$el.index()].txt + '<button class="dismiss callend">Fechar</button>');
+      },
+      callEnd: function() {
+        var i, j, k, len, ref, results;
+        if (clickarea.ctrl.length !== clickarea.data.length) {
+          return false;
+        } else {
+          ref = clickarea.ctrl;
+          results = [];
+          for (j = k = 0, len = ref.length; k < len; j = ++k) {
+            i = ref[j];
+            if (clickarea.ctrl.indexOf(i) === clickarea.resp[j]) {
+              results.push(setTimeout(function() {
+                return func.end();
+              }, 1000));
+            } else {
+              results.push(void 0);
+            }
+          }
+          return results;
         }
       }
     };
@@ -76,17 +157,26 @@
         return $('.dimmer').fadeOut();
       },
       start: function() {
+        sets.sound = false;
+        audio.start();
+        sets.clickable = true;
+        clickarea.start();
         func.dismiss();
-        sound = true;
-        audio.trilha.volume = 0.6;
-        audio.trilha.loop = true;
-        audio.trilha.play();
-        audio.clique.play();
         return $('.content').fadeIn();
       }
     };
     $(document).on('click', '.audio', function() {
       return audio.audio();
+    });
+    $(document).on('click', '.clickarea *', function() {
+      if (sets.clickable === true) {
+        return clickarea.showC($(this));
+      }
+    });
+    $(document).on('click', '.dismiss', function() {
+      if (sets.clickable === true) {
+        return clickarea.callEnd();
+      }
     });
     $(document).on('click', '.start', function() {
       return func.start();
